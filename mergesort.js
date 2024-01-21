bbef568dace8200d1a60a944d8a760cc32477c8c
Yahya Islamovic <yahya.islamovic.work@live.com>
2024-01-21 00:50:48 -0500

    debug logs

--- mergesort.js
+++ mergesort.js
@@ -43,19 +43,21 @@ function mergeSortedArrays(n, m) {
     return arr;
 }
 
-let mostlySortedDataset = generateMostlySortedDataset(100);
-let randomlyShuffledDataset =   generateRandomDataset(100);
+let mostlySortedDataset = generateMostlySortedDataset(100000);
+let randomlyShuffledDataset =   generateRandomDataset(100000);
 
 console.log("\n")
 console.log("Mostly sorted data:")
 const start = performance.now();
 sort(mostlySortedDataset)
 const end = performance.now();
+//console.log(sort(mostlySortedDataset));
 console.log(`Execution time: ${(end - start).toFixed(2)} ms`);
 console.log("\n")
 console.log("Shuffled data:")
 const start2 = performance.now();
 sort(randomlyShuffledDataset)
+//console.log(sort(randomlyShuffledDataset));
 const end2 = performance.now();
 console.log(`Execution time: ${(end2 - start2).toFixed(2)} ms`);
 console.log("\n")
