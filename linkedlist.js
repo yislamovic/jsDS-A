f60500e762b4406894ff2c99845eb391909d41ae
Yahya Islamovic <yahya.islamovic.work@live.com>
2024-02-25 13:49:34 -0500

    shift method

--- linkedlist.js
+++ linkedlist.js
@@ -22,4 +22,41 @@ class SinglyLinkedList{
     }
     this.length += 1;
   }
+  pop(){
+    if(!this.head) return undefined;
+    let current = this.head;
+    let previousNode = null;
+    while(current.next){
+      previousNode = current;
+      current = current.next;
+    }
+    this.tail = previousNode;
+    this.tail.next = null;
+    this.length -= 1;
+    if(this.length === 0){
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift(){
+    if(!this.head) return undefined;
+    let head = this.head;
+    this.head = this.head.next;
+    this.length -= 1;
+    if(this.length === 0){
+      this.tail = null;
+    }
+    return head;
+  }
 }
+
+let list = new SinglyLinkedList();
+list.push("a");
+list.push("b");
+list.push("c");
+// console.log(list);
+// console.log(list.pop());
+// console.log(list);
+console.log(list.shift());
+console.log(list);
