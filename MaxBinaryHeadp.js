d37a3a2291d36d775ce5e062b0a364cd5046e8c7
Yahya Islamovic <yahya.islamovic.work@live.com>
2024-04-23 16:09:48 -0400

    insert func on a max binary heap

---
+++ MaxBinaryHeadp.js
@@ -0,0 +1,30 @@
+class MaxBinaryHeap{
+    constructor(val){
+        this.values = val;
+    }
+    insert(val){
+        this.values.push(val);
+        var index = this.values.length - 1;
+        while(index > 0){
+            var parentIndex = Math.floor((index - 1) / 2);
+            if(this.values[parentIndex] < val){
+                var temp = this.values[parentIndex];
+                this.values[parentIndex] = val;
+                this.values[index] = temp;
+                index = parentIndex;
+            } else break;
+        }
+    }
+}
+//var heap = new MaxBinaryHeap([100,90,80,70,50,60,30,20,10,40]);
+var heap = new MaxBinaryHeap([41,39,33,18,27,12]);
+//heap.insert(101);
+//heap.insert(71);
+heap.insert(55);
+heap.insert(1);
+heap.insert(45);
+heap.insert(199);
+
+
+console.log(heap.values);
+console.log("END")
